400036264dc76c836a9f5e6a1ebb137e232ed1c3
PolinaTsugankova <pp6056512@gmail.com>
2023-07-23 11:10:10 +0300

    Update app

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,12 +1,9 @@
 import { useSelector } from 'react-redux';
 import { Navigate, Outlet } from 'react-router-dom';
-import { getIsLoggedIn, getIsRefreshing } from './../redux/auth/selectors';
+import { getIsLoggedIn } from './../redux/auth/selectors';
 
 export default function PrivateRoute({ redirectTo = '/' }) {
   const isLoggedIn = useSelector(getIsLoggedIn);
-//   const isRefreshing = useSelector(getIsRefreshing);
-
-//   const shouldRedirect = !isLoggedIn && !isRefreshing;
 
   return isLoggedIn ? <Outlet /> : <Navigate to={redirectTo} />;
 }
--- src/components/PublicRoute.jsx
+++ src/components/PublicRoute.jsx
@@ -1,11 +1,9 @@
 import { useSelector } from 'react-redux';
 import { Navigate, Outlet } from 'react-router-dom';
-import { getIsLoggedIn, getIsRefreshing } from './../redux/auth/selectors';
+import { getIsLoggedIn } from './../redux/auth/selectors';
 
 export default function PublicRoute({ restricted = false, redirectTo = '/' }) {
   const isLoggedIn = useSelector(getIsLoggedIn);
-  //   const isRefreshing = useSelector(getIsRefreshing);
-
   const shouldRedirect = isLoggedIn && restricted;
 
   return shouldRedirect ? <Navigate to={redirectTo} /> : <Outlet />;
